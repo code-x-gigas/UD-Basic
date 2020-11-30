559a92f56c8013b6cd440f508ed67dbfc096aeae
Cyber-Naraboji <73358232+Cyber-Naraboji@users.noreply.github.com>
2020-11-30 10:15:47 +0530

    Add files via upload

---
+++ banner.sh
@@ -0,0 +1,34 @@
+#!/system/bin/sh
+red='\033[1;31m'
+white='\033[1;37m'
+green='\033[1;32m'
+cyan='\033[96m'
+yellow='\033[33m'
+blue='\033[94m'
+clear
+printf "\n"
+printf "\n"
+echo " Created By - Cyber Naraboji | Grey Hatter | Kaalante Doodhan"| lolcat -a -d 50
+printf "\n"
+printf $green"  ██╗   ██╗██████╗       ██████╗  █████╗ ███████╗██╗ ██████╗ \n "
+printf $green" ██║   ██║██╔══██╗      ██╔══██╗██╔══██╗██╔════╝██║██╔════╝ \n "
+printf $green" ██║   ██║██║  ██║      ██████╔╝███████║███████╗██║██║ \n "
+printf $green" ██║   ██║██║  ██║      ██╔══██╗██╔══██║╚════██║██║██║ \n "
+printf $green" ╚██████╔╝██████╔╝      ██████╔╝██║  ██║███████║██║╚██████╗ \n "
+printf $green"  ╚═════╝ ╚═════╝      ╚══════╝ ╚═╝    ╚═══════╝  ╚═══════╝\n "
+printf $blue"                                                 version 0.0.1 \n"
+printf $green"            \n "
+printf $cyan"     ━━━━━━━━━━━━━━━["
+printf $yellow" Undercoding Devils "
+printf $cyan" ]━━━━━━━━━━━━━━━ \n "
+printf "\n"
+printf $white"          +--------------------------------------+ \n "
+printf $white"         |"$red" This tool install all basic packages"
+printf $white" |\n"
+printf $white" +------------------------------------------------------------+"
+printf "\n"
+printf $white" |"$red" Coded By - Cyber Naraboji | Kaalante Doodhan | Grey Hatter"
+printf $white" |\n"
+printf $white" +------------------------------------------------------------+ \n"
+printf "\n"
+
